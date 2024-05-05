89d457669079e91580b44a4c264b41ae29a58268
unknown <511268932@qq.com>
2024-05-05 16:00:03 +0800

    interator

--- iterator.go
+++ iterator.go
@@ -1,6 +1,8 @@
 package largecache
 
-import "sync"
+import (
+	"sync"
+)
 
 type iteratorError string
 
@@ -39,12 +41,94 @@ func (e EntryInfo) Value() []byte {
 }
 
 type EntryInfoIterator struct {
-	mutex sync.Mutex
-	//cache *
+	mutex           sync.Mutex
+	cache           *LargeCache
 	currentShard    int
 	currentIndex    int
 	curentEntryInfo EntryInfo
 	elements        []uint64
 	elementsCount   int
 	valid           bool
+}
+
+func (it *EntryInfoIterator) SetNext() bool {
+	it.mutex.Lock()
+
+	it.valid = false
+	it.currentIndex++
+
+	if it.elementsCount > it.currentIndex {
+		it.valid = true
+
+		empty := it.setCurrentEntry()
+		it.mutex.Unlock()
+
+		if empty {
+			return it.SetNext()
+		}
+		return true
+	}
+
+	for i := it.currentShard + 1; i < it.cache.config.Shards; i++ {
+		it.elements, it.elementsCount = it.cache.shards[i].copyHashedKeys()
+
+		if it.elementsCount > 0 {
+			it.currentIndex = 0
+			it.currentShard = i
+			it.valid = true
+
+			empty := it.setCurrentEntry()
+			it.mutex.Unlock()
+
+			if empty {
+				return it.SetNext()
+			}
+			return true
+		}
+	}
+	it.mutex.Unlock()
+	return false
+}
+
+func (it *EntryInfoIterator) setCurrentEntry() bool {
+	var entryNotFound = false
+	entry, err := it.cache.shards[it.currentShard].getEntry(it.elements[it.currentIndex])
+
+	if err == ErrEntryNotFound {
+		it.curentEntryInfo = emptyEntryInfo
+		entryNotFound = true
+	} else if err != nil {
+		it.curentEntryInfo = EntryInfo{
+			err: err,
+		}
+	} else {
+		it.curentEntryInfo = EntryInfo{
+			timestamp: readHashFromEntry(entry),
+			hash:      readHashFromEntry(entry),
+			key:       readKeyFromEntry(entry),
+			value:     readEntry(entry),
+			err:       err,
+		}
+	}
+
+	return entryNotFound
+}
+
+func newInterator(cache *LargeCache) *EntryInfoIterator {
+	elements, count := cache.shards[0].copyHashedKeys()
+
+	return &EntryInfoIterator{
+		cache:         cache,
+		currentShard:  0,
+		currentIndex:  -1,
+		elements:      elements,
+		elementsCount: count,
+	}
+}
+
+func (it *EntryInfoIterator) value() (EntryInfo, error) {
+	if !it.valid {
+		return emptyEntryInfo, ErrInvalidIteratorState
+	}
+	return it.curentEntryInfo, it.curentEntryInfo.err
 }
